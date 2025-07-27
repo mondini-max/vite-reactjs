b1970f4bfa65ce572e8384bec97ff9640c82ea0a
Maxime Mondini <mh-mondini@hotmail.com>
2025-07-27 12:24:00 +0200

    Add empty cart message in CartContainer component

--- vite-app/src/components/CartContainer.jsx
+++ vite-app/src/components/CartContainer.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import CartItem from './CartItem.jsx';
 
 const CartContainer = ({ cart = [] }) => {
+  if (cart.length === 0) {
+    return (
+      <section className='cart'>
+        <header>
+          <h2>Your Cart</h2>
+          <h4 className='empty-cart'>is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
   return (
     <section className='cart'>
       <header>
